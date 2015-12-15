2ab55971c220d2f9378e27befcb22cfbdc1af094
Meyer, Walter <walter.meyer@purchase.edu>
2015-12-15 12:14:53 -0500

    first test

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+# Dockerfile that sets up a build environment for compiling PF_RING for CoreOS
+#
+# On push, Travis-CI takes care of building this image, running the container,
+# and pushing all of the compiled drivers to a (tagged) branch in GitHub.
+# 
+# Last Updated: 12/14/2015
+# CoreOS Stable: 835.9.0
+# CoreOS Beta: 877.1.0
+# CoreOS Alpha: 891.0.0
+#
+# This Dockerfile is just for the pf_ring kernel module/driver build automation.
+# Another container (e.g. snort, suricata, bro, etc.), should pull down said
+# precompiled modules at runtime and activate them using 'insmod'.
+#
+
+FROM debian:jessie
+MAINTAINER Walter Meyer <wgmeyer@gmail.com>
+
+# PF_RING Version
+ENV pf_ring_version 6.2.0-stable
+
+# Setup environment
+RUN apt-get -y update && apt-get -y \
+	install gcc-4.9 bc wget jq git make dpkg-dev module-init-tools && \
+    mkdir -p /usr/src/kernels && \
+    mkdir -p /opt/pfring && \
+    apt-get autoremove && apt-get clean && \
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 10
+
+# Download kernel source
+RUN cd /usr/src/kernels && \
+    git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git linux
+
+# Get pfring source
+RUN git clone https://github.com/ntop/PF_RING.git /opt/pfring && \
+	cd /opt/pfring && git checkout -b $pf_ring_version
+
+ADD entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Last Updated: 12/14/2015
+# CoreOS Stable: 835.9.0
+# CoreOS Beta: 877.1.0
+# CoreOS Alpha: 891.0.0
+
+# Get the CoreOS kernel version we want to compile against from $release env var
+kernel=$(wget -qO- https://coreos.com/releases/releases.json | \
+jq --arg release $release .['$release] | ."major_software" | ."kernel"| .[0]' |
+tr -d '"')
+
+# Compilation preparation
+cd /usr/src/kernels/linux
+git checkout -b stable v$kernel
+zcat /proc/config.gz > .config
+make modules_prepare
+sed -i "s/$kernel/$kernel-coreos-r1/g" include/generated/utsrelease.h
+mkdir -p /lib/modules/$kernel-coreos-r1/
+ln -s /usr/src/kernels/linux/ /lib/modules/$kernel-coreos-r1/build
+
+# Compilation: pf_ring kernel module
+cd /opt/pfring/kernel && make
+
+# Compilation: pf_ring drivers
+cd /opt/pfring/drivers/ZC && make
+
+# Output directories
+mkdir -p /builds/{kernel,drivers}
+mkdir -p /builds/drivers/ZC/intel/{e1000e,igb,ixgbe,i40e}
+
+# kernel
+cp /opt/pfring/kernel/pf_ring.ko /builds/kernel/
+# e1000e
+cp /opt/pfring/drivers/ZC/intel/e1000e/e1000e-*/src/e1000e.ko /builds/drivers/ZC/intel/e1000e/
+# igb
+cp /opt/pfring/drivers/ZC/intel/igb/igb-*/src/igb.ko /builds/drivers/ZC/intel/igb/
+# ixgbe
+cp /opt/pfring/drivers/ZC/intel/ixgbe/ixgbe-*/src/ixgbe.ko /builds/drivers/ZC/intel/ixgbe/
+# i40e
+cp /opt/pfring/drivers/ZC/intel/i40e/i40e-*/src/i40e/i40e.ko /builds/drivers/ZC/intel/i40e/
