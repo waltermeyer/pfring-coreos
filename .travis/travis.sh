de66157c409495b23987b6465749f859f4830f4f
Meyer, Walter <walter.meyer@purchase.edu>
2015-12-16 17:08:18 -0500

    still goin...2

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-wget http://stable.release.core-os.net/amd64-usr/$RELEASE/coreos_developer_container.bin.bz2
+wget http://$CHANNEL.release.core-os.net/amd64-usr/$RELEASE/coreos_developer_container.bin.bz2
 bunzip2 coreos_developer_container.bin.bz2
 sudo kpartx -asv coreos_developer_container.bin
 LOOPDEV=$(sudo kpartx -asv coreos_developer_container.bin | cut -d\  -f 3)
 sudo mkdir /tmp/loop || true
 sudo mount /dev/mapper/$LOOPDEV /tmp/loop
-cp /tmp/loop/usr/boot/config-* .
+cp /tmp/loop/usr/boot/config .config
 sudo umount /tmp/loop
 sudo kpartx -dv coreos_developer_container.bin
 rm -rf coreos_developer_container.bin
-cp config-* .config
