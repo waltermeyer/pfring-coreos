75bc8c6d0d960b59201f56afd08bd3f332f7d7fa
Meyer, Walter <walter.meyer@purchase.edu>
2015-12-21 10:36:36 -0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,33 +26,33 @@ cp linux/pf_ring.h /usr/src/kernels/linux/include/linux/
 make -C /usr/src/kernels/linux/ M=$PWD
 
 # Compilation: pf_ring drivers
-cd /opt/pfring/drivers/PF_RING_aware/intel/i40e/i40e-*/src/i40e/
+cd /opt/pfring/drivers/ZC/intel/i40e/i40e-*/src/i40e/
 make -C /usr/src/kernels/linux/ M=$PWD
 
-cd /opt/pfring/drivers/PF_RING_aware/intel/igb/igb-*/src/
+cd /opt/pfring/drivers/ZC/intel/igb/igb-*/src/
 make -C /usr/src/kernels/linux/ M=$PWD
 
-cd /opt/pfring/drivers/PF_RING_aware/intel/e1000e/e1000e-*/src/
+cd /opt/pfring/drivers/ZC/intel/e1000e/e1000e-*/src/
 make -C /usr/src/kernels/linux/ M=$PWD
 
-cd /opt/pfring/drivers/PF_RING_aware/intel/ixgbe/ixgbe-*/src/
+cd /opt/pfring/drivers/ZC/intel/ixgbe/ixgbe-*/src/
 make -C /usr/src/kernels/linux/ M=$PWD
 
 
 # Output directories
 mkdir -p /builds/kernel/
-mkdir -p /builds/drivers/PF_RING_aware/intel/e1000e/
-mkdir -p /builds/drivers/PF_RING_aware/intel/igb/
-mkdir -p /builds/drivers/PF_RING_aware/intel/ixgbe/
-mkdir -p /builds/drivers/PF_RING_aware/intel/i40e/
+mkdir -p /builds/drivers/ZC/intel/e1000e/
+mkdir -p /builds/drivers/ZC/intel/igb/
+mkdir -p /builds/drivers/ZC/intel/ixgbe/
+mkdir -p /builds/drivers/ZC/intel/i40e/
 
 # kernel
 cp /opt/pfring/kernel/pf_ring.ko /builds/kernel/
 # e1000e
-cp /opt/pfring/drivers/PF_RING_aware/intel/e1000e/e1000e-*/src/e1000e.ko /builds/drivers/PF_RING_aware/intel/e1000e/
+cp /opt/pfring/drivers/ZC/intel/e1000e/e1000e-*/src/e1000e.ko /builds/drivers/ZC/intel/e1000e/
 # igb
-cp /opt/pfring/drivers/PF_RING_aware/intel/igb/igb-*/src/igb.ko /builds/drivers/PF_RING_aware/intel/igb/
+cp /opt/pfring/drivers/ZC/intel/igb/igb-*/src/igb.ko /builds/drivers/ZC/intel/igb/
 # ixgbe
-cp /opt/pfring/drivers/PF_RING_aware/intel/ixgbe/ixgbe-*/src/ixgbe.ko /builds/drivers/PF_RING_aware/intel/ixgbe/
+cp /opt/pfring/drivers/ZC/intel/ixgbe/ixgbe-*/src/ixgbe.ko /builds/drivers/ZC/intel/ixgbe/
 # i40e
-cp /opt/pfring/drivers/PF_RING_aware/intel/i40e/i40e-*/src/i40e/i40e.ko /builds/drivers/PF_RING_aware/intel/i40e/
+cp /opt/pfring/drivers/ZC/intel/i40e/i40e-*/src/i40e/i40e.ko /builds/drivers/ZC/intel/i40e/
